f7bdcccd8c5fbfe99a5f9e02750080a05526d0ab
katseyes <thibof@hotmail.com>
2022-02-07 18:59:33 +0100

    Updates

---
+++ precache-manifest.90b6f86b2269bc673859813cb57ba19c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ebad33c5619484a606b5de89d5a130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ec4d13256869394e89",
+    "url": "/static/css/2.6a23f8e0.chunk.css"
+  },
+  {
+    "revision": "67765da408e1f553c96f",
+    "url": "/static/css/main.9dfdaa36.chunk.css"
+  },
+  {
+    "revision": "84ec4d13256869394e89",
+    "url": "/static/js/2.c31ac833.chunk.js"
+  },
+  {
+    "revision": "67765da408e1f553c96f",
+    "url": "/static/js/main.10abcaa5.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "fb666fbbba79266577c1547543666976",
+    "url": "/static/media/Ethereum_logo_2014.fb666fbb.svg"
+  },
+  {
+    "revision": "a89a5c8cdfd17774c464b4afba5b7786",
+    "url": "/static/media/dtube.a89a5c8c.png"
+  }
+]);
